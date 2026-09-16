cb3955829de99f2275ff61595a4fbbb3739138af
Anu <anu705545@gmail.com>
2026-09-16 18:29:17 +0530

    commit

--- OOPS Python/oops_project.py
+++ OOPS Python/oops_project.py
@@ -18,10 +18,10 @@ class Chatbook:
         """)
 
         if user_input == "1":
-            pass
+            self.signup()
 
         elif user_input == "2":
-            pass
+            self.signin()
 
         elif user_input == "3":
             pass
@@ -32,6 +32,25 @@ class Chatbook:
         else:
             exit()
 
+    def signup(self):
+        email =input("Enter your email here..")
+        pwd = input("setup your password here..")
+        self.username=email
+        self.password=pwd
+        print("You have signed successfully!!")
+        print("\n")
+        self.menu()
 
+    def signin(self):
+        if self.username=="" and self.password=="":
+            print("Please signup first by pressing 1 in the menu")
+        else:
+            uname=input("Enter your email/username here..")
+            pwd = input("Enter your password here..")
+            if self.username==uname and self.password==pwd:
+                print("You have signed in Successfully!!")
+                self.loggedin =True
+            else:
+                print("Please input correct Credential")
 obj = Chatbook()
 
