46e91fead6b708fcda700e82116bf364f2fd1739
Anu <anu705545@gmail.com>
2026-09-21 15:46:00 +0530

    third version of data saved

--- DVC-Data Versioning/mycode.py
+++ DVC-Data Versioning/mycode.py
@@ -13,8 +13,8 @@ new_row_loc = {"Name":"New_person","age":20,"city":"city1"}
 df.loc[len(df.index)] = new_row_loc
 
 # # Adding new row to df for v3
-# new_row_loc2= {"Name":"v3", "age":30,"city":"city1"}
-# df.loc[len(df.index)] = new_row_loc2
+new_row_loc2= {"Name":" new_person2", "age":30,"city":"city1"}
+df.loc[len(df.index)] = new_row_loc2
 
 # Ensure the "data " directory exists at the root level
 data_dir ="data"
