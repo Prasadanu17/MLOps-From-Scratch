d7e0bf77c2a16b09ac7003058a0843f0718589bc
Anu <anu705545@gmail.com>
2026-09-21 15:36:09 +0530

    second version of data saved

--- DVC-Data Versioning/mycode.py
+++ DVC-Data Versioning/mycode.py
@@ -9,8 +9,8 @@ data = {"Name":["anu","sus","charlie"],
 df =pd.DataFrame(data)
 
 # # Adding new row to df for v2
-# new_row_loc = {"Name":"v2","age":20,"city":"city1"}
-# df.loc[len(df.index)] = new_row_loc
+new_row_loc = {"Name":"New_person","age":20,"city":"city1"}
+df.loc[len(df.index)] = new_row_loc
 
 # # Adding new row to df for v3
 # new_row_loc2= {"Name":"v3", "age":30,"city":"city1"}
